6678b2708957801cfe0dcf2f17da10c1bd934e9a
Zhenya Telyukov <telyukov@gmail.com>
2015-12-19 14:25:30 +0300

    master updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,13 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//  https://github.com/twbs/bootstrap-sass:
+//  bootstrap-sprockets and bootstrap SHOULD NOT BOTH BE INCLUDED in application.js
+//  - bootstrap-sprockets provides individual Bootstrap Javascript files (alert.js or dropdown.js, for example)
+//  - bootstrap provides a concatenated file containing all Bootstrap Javascripts.
+
 //= require jquery
 //= require jquery_ujs
-// require turbolinks
+//= require turbolinks
 //= require bootstrap
-// require rainbow
-// require ruby
 //= require_tree .
